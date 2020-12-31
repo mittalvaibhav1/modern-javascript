8a43631753938bba2bb23ef29b45a0ac47c92f77
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-31 23:14:36 +0530

    .innerHTML

--- JavaScript Basics/sandbox.js
+++ JavaScript Basics/sandbox.js
@@ -1,24 +1,54 @@
-const para = document.querySelector('p'); //returns a single element
-//Returns the first element that matches the query
-console.log(para);
+//DOM  Manipulation
 
-const para2 = document.querySelector('p.error'); //filter by class 
-console.log(para2);
+const para = document.querySelector('p');
+console.log(para.innerHTML);
 
-const allParas = document.querySelectorAll('p'); //returns multiple elements 
-//Returns all the elements that matches the query
-console.log(allParas);
+para.innerHTML = 'Amazing hehe'
 
-allParas.forEach((para) => console.log(para)); //NodeList Array...
+const paras = document.querySelectorAll('p');
 
-//Other Methods... 
+paras.forEach((para) => {
+    console.log(para.innerHTML);
+    para.innerHTML += '  wheeeeeeeee' 
+})
 
-const title = document.getElementById('page-title'); //returns a single element with given ID
-console.log(title);
+const content = document.querySelector('div.error');
+console.log(content.innerHTML);
+content.innerHTML += "<h2> Hello, How are you today?";
 
-const errors = document.getElementsByClassName('error'); //returns an array of HTMLCollection with given class.
-console.log(errors);
-//errors.forEach((error) => console.log(error));// Cannot use forEach with HTMLCollection. Will give an error.
+const people = ['mario','luigi','yoshi'];
 
-const paras = document.getElementsByTagName('p');
-console.log(paras);
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`
+})
+
+
+
+//To Query the DOM
+
+// const para = document.querySelector('p'); //returns a single element
+// //Returns the first element that matches the query
+// console.log(para);
+
+// const para2 = document.querySelector('p.error'); //filter by class 
+// console.log(para2);
+
+// const allParas = document.querySelectorAll('p'); //returns multiple elements 
+// //Returns all the elements that matches the query
+// console.log(allParas);
+
+// allParas.forEach((para) => console.log(para)); //NodeList Array...
+
+// //Other Methods... 
+
+// const title = document.getElementById('page-title'); //returns a single element with given ID
+// console.log(title);
+
+// const errors = document.getElementsByClassName('error'); //returns an array of HTMLCollection with given class.
+// console.log(errors);
+// //errors.forEach((error) => console.log(error));// Cannot use forEach with HTMLCollection. Will give an error.
+
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+
+
