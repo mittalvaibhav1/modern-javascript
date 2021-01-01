d9c1b538f7cf3a4c383bbb059d90f7df185b75a9
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-02 01:15:58 +0530

    keyup

--- Form Events/sandbox.js
+++ Form Events/sandbox.js
@@ -3,17 +3,27 @@ const form = document.querySelector('.signup-form');
 //can get reference to all the fields if they have an ID or name, using the reference to the form!
 const feedback = document.querySelector('.feedback');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const username = form.username.value;
+form.username.addEventListener('keyup', (e) => {
     const pattern = /^[a-zA-Z]{6,12}$/;
-    if(pattern.test(username)) {
-        feedback.innerHTML = 'yeh bhe theek hai';
-    } else {
-        feedback.innerHTML = 'arey mat karooooo';
+    if(!pattern.test(e.target.value)) {
+        e.target.style.backgroundColor = 'rgba(220,60,20,0.5)';
     }
-
+    else {
+        e.target.style.backgroundColor = 'rgba(0,250,154,0.5)';
+    }
 });
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const username = form.username.value;
+//     const pattern = /^[a-zA-Z]{6,12}$/;
+//     if(pattern.test(username)) {
+//         feedback.innerHTML = 'yeh bhe theek hai';
+//     } else {
+//         feedback.innerHTML = 'arey mat karooooo';
+//     }
+
+// });
 
 // const username = 'hihellolisten'
 // const pattern = /^[a-z]{6,}$/;
