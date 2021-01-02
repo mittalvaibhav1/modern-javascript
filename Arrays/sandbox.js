fd96ef28309a71c534d92c2aaadb7930da836b35
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-03 01:19:41 +0530

    map

--- Arrays/sandbox.js
+++ Arrays/sandbox.js
@@ -1,20 +1,50 @@
-const scores = [10, 30, 15, 25, 50, 40 ,5];
+//! Map
 
-const res = scores.filter((item) => {
-    return item >= 20; // return true if you want to keep it
+const prices = [20, 0, 30, 25, 15, 40, 80, 5];
+
+const salePrices = prices.map((price) => {
+    return price / 2;
 });
 
-console.log(scores,res);
+console.log(salePrices);
 
-const users = [
-    {name: 'shaun' , premium: true},
-    {name: 'yoshi' , premium: false},
-    {name: 'mario' , premium: false},
-    {name: 'chun-li' , premium: true}
-]
+const products = [
+    {name: 'gold star' , price: 20},
+    {name: 'mushroom' , price: 40},
+    {name: 'green sheels' , price: 30},
+    {name: 'banana skin' , price: 10},
+    {name: 'red shells' , price: 50}
+];
 
-const premiumUsers = users.filter((user) => {
-    return user.premium;
+const saleProducts = products.map((product) => {
+    return {
+        ...product,
+        price : product.price > 30 ? product.price / 2 : product.price
+    }
 })
 
-console.log(premiumUsers);
+console.log(saleProducts);
+
+//! Filter
+
+// const scores = [10, 30, 15, 25, 50, 40 ,5];
+
+// const res = scores.filter((item) => {
+//     return item >= 20; // return true if you want to keep it
+// });
+
+// console.log(scores,res);
+
+// const users = [
+//     {name: 'shaun' , premium: true},
+//     {name: 'yoshi' , premium: false},
+//     {name: 'mario' , premium: false},
+//     {name: 'chun-li' , premium: true}
+// ]
+
+// const premiumUsers = users.filter((user) => {
+//     return user.premium;
+// })
+
+// console.log(premiumUsers);
+
