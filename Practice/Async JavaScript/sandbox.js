f624c6cefee70a50e4ba55806e8a5d3f4487918a
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-04 01:34:15 +0530

    Fetch API

--- Practice/Async JavaScript/sandbox.js
+++ Practice/Async JavaScript/sandbox.js
@@ -1,30 +1,48 @@
 
+//Fetch API
+
+fetch('https://jsonplaceholder.typicode.com/todos/')
+.then((response) => {
+    if(response.ok) {
+        return response;
+    }
+    else {
+        let error = Error('Error: ' + response.status + ' ' + response.statusText);
+        error.response = response;
+        throw error;
+    }
+})
+.then(response => response.json())
+.then(response => console.log(response))
+.catch(error => console.log(error.message));
+
+
 // Promises
-const getTodos = () => {
-    return new Promise((resolve,reject) => {
+// const getTodos = () => {
+//     return new Promise((resolve,reject) => {
 
-        const request = new XMLHttpRequest();
-        request.open('GET','https://jsonplaceholder.typicode.com/todos/');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if(request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            }
-            else if(request.readyState === 4) {
-                reject('could not fetch data');
-            }
-        });
-    })
-}
+//         const request = new XMLHttpRequest();
+//         request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+//         request.send();
+//         request.addEventListener('readystatechange', () => {
+//             if(request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data);
+//             }
+//             else if(request.readyState === 4) {
+//                 reject('could not fetch data');
+//             }
+//         });
+//     })
+// }
 
-getTodos()
-.then(response => {
-    console.log("response",response);
-})
-.catch(error => {
-    console.log(error);
-})
+// getTodos()
+// .then(response => {
+//     console.log("response",response);
+// })
+// .catch(error => {
+//     console.log(error);
+// })
 
 // const request = new XMLHttpRequest();
 
