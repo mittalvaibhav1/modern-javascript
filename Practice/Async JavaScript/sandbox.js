749ac01afd67f6bfaf373f168b687114d158a53f
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-04 00:32:44 +0530

    callbacks

--- Practice/Async JavaScript/sandbox.js
+++ Practice/Async JavaScript/sandbox.js
@@ -1,15 +1,40 @@
-const request = new XMLHttpRequest();
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
 
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
-    } else if(request.readyState === 4){
-        console.log('Nahi');
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200) {
+            callback(undefined,request.responseText);
+        }
+        else if(request.readyState === 4) {
+            callback('could not fetch data' , undefined);
+        }
+    });
+}
+
+getTodos((err , data) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(data)
     }
 });
+
+// const request = new XMLHttpRequest();
+
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+// request.send();
+
+// request.addEventListener('readystatechange', () => {
+//     if(request.readyState === 4 && request.status === 200) {
+//         console.log(request.responseText);
+//     } else if(request.readyState === 4){
+//         console.log('Nahi');
+//     }
+// });
 
 // const request = new XMLHttpRequest();
 
