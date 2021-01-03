c529e61b7258be0d169a941371c7c03dfe15bca6
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-03 16:10:37 +0530

    clock

--- Practice/Date and Time/sandbox.js
+++ Practice/Date and Time/sandbox.js
@@ -1,54 +1,71 @@
+const clock = document.querySelector('.clock');
+
+const tick = () => {
+    const now = new Date();
+    const h = now.getHours();
+    const m = now .getMinutes();
+    const s = now.getSeconds();
+    const html = `
+        <span>${h}</span> : 
+        <span>${m}</span> : 
+        <span>${s}</span>
+    `
+    clock.innerHTML = html;
+}
+
+setInterval(tick,1000);
+
 //! Date and time
-const now = new Date();
-console.log(now); // Date and Time both!
+// const now = new Date();
+// console.log(now); // Date and Time both!
 
-//* year , months , day , time , date
+// //* year , months , day , time , date
 
-//year
-console.log(now.getFullYear());
+// //year
+// console.log(now.getFullYear());
 
-//month
-console.log(now.getMonth());
+// //month
+// console.log(now.getMonth());
 
-//day 
-console.log(now.getDay());
+// //day 
+// console.log(now.getDay());
 
-//timestamp
-console.log(now.getTime()); //number of miliseconds since 1st Jan 1970
+// //timestamp
+// console.log(now.getTime()); //number of miliseconds since 1st Jan 1970
 
-//date
-console.log(now.getDate());
+// //date
+// console.log(now.getDate());
 
-//hours
-console.log(now.getHours());
+// //hours
+// console.log(now.getHours());
 
-//minutes
-console.log(now.getMinutes());
+// //minutes
+// console.log(now.getMinutes());
 
-//seconds
-console.log(now.getSeconds());
+// //seconds
+// console.log(now.getSeconds());
 
-//* date strings
+// //* date strings
 
-console.log(now.toDateString()); // Only Date
+// console.log(now.toDateString()); // Only Date
 
-console.log(now.toTimeString()); // Only Time
+// console.log(now.toTimeString()); // Only Time
 
-console.log(now.toLocaleString()); // Dat and time in local format.
+// console.log(now.toLocaleString()); // Dat and time in local format.
 
-// Dates
+// // Dates
 
-const before = new Date('Jan 3 2020 15:59:59');
-console.log(before);
+// const before = new Date('Jan 3 2020 15:59:59');
+// console.log(before);
 
-const diff = now.getTime() - before.getTime(); //In miliseconds
-console.log(diff);
+// const diff = now.getTime() - before.getTime(); //In miliseconds
+// console.log(diff);
 
-const mins = Math.round(diff / 1000 / 60);
-console.log(mins);
+// const mins = Math.round(diff / 1000 / 60);
+// console.log(mins);
 
-const hours = Math.round(mins / 60);
-console.log(hours);
+// const hours = Math.round(mins / 60);
+// console.log(hours);
 
-const days = Math.round(hours / 24);
-console.log(days);
+// const days = Math.round(hours / 24);
+// console.log(days);
