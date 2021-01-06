ddc377a286f1bbc952f1c892383e71d4295b218d
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-07 01:48:35 +0530

    Spread

--- Practice/Others/sandox.js
+++ Practice/Others/sandox.js
@@ -8,4 +8,26 @@ const double = (...nums) => {
 
 console.log(double(1,2,3,4,5,6,7,8,9,10));
 
+// Spread syntax (Arrays)
+// Spread them into their individual components
+
+const people = ['shaun' , 'ryu' , 'crystal'];
+
+console.log(...people);
+
+const memebers = ['mario' , 'luigi', ...people];
+
+console.log(memebers);
+
+// Spread syntax (Objects)
+
+const person = {
+    name : 'shaun',
+    age: 30,
+    job: 'net'
+}
+
+const personClone = {...person}; // Entirely new copy!
+
+console.log(personClone);
 
