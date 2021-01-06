ee2af0f975df267453bd3f30eb4cf0e35ca89774
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-07 01:27:53 +0530

    onSnapshot

--- Practice/firebase/sandbox.js
+++ Practice/firebase/sandbox.js
@@ -13,19 +13,37 @@ const addRecipe = (recipe,id) => {
     list.innerHTML += html;
 }
 
-//Real time listeners
+const deleteRecipe = (id) => {
+    const recipie = document.querySelector(`[data-id='${id}']`);
+    if(recipie) recipie.remove();
+}
+//Real time listeners // onSnapshot() // Whenever data changes
+//Firebase takes a snapshot. Thus onSnapshot will be executed.
+
+db.collection('recipies').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach((change) => {
+        const doc = change.doc;
+        if(change.type === 'added') {
+            addRecipe(doc.data(),doc.id);
+        }
+        else if(change.type === 'removed'){
+            deleteRecipe(doc.id);
+        }
+    })
+})
 
 
-// Get Document 
-db.collection('recipies').get().then((snapshot) => {
-    //When we ge tthe data
-    snapshot.docs.forEach((doc) =>{
-        addRecipe(doc.data(),doc.id);
-    });
-})
-.catch((err) => {
-    console.log(err.message);
-})
+
+// Get Document // Fetches 1 time. We need it to be realtime 
+// db.collection('recipies').get().then((snapshot) => {
+//     //When we ge tthe data
+//     snapshot.docs.forEach((doc) =>{
+//         addRecipe(doc.data(),doc.id);
+//     });
+// })
+// .catch((err) => {
+//     console.log(err.message);
+// })
 
 // Add document
 form.addEventListener('submit',(e) => {
@@ -35,8 +53,9 @@ form.addEventListener('submit',(e) => {
         title : form.recipe.value,
         created_at : firebase.firestore.Timestamp.fromDate(now)
     }
+    form.reset();
     db.collection('recipies').add(recipe).then(() => {
-
+        
     }).catch(err => {
         console.log(err.message);
     })
@@ -46,7 +65,6 @@ form.addEventListener('submit',(e) => {
 list.addEventListener('click',(e) => {
     if(e.target.tagName === 'BUTTON') {
         const id = e.target.parentElement.getAttribute('data-id');
-        console.log(id);
         db.collection('recipies').doc(id).delete().then(() => {
             console.log('deleted');
         }).catch(err => console.log(err.message));
